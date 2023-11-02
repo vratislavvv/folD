3fd5cca7102cae5c351b6bda4f8bd700d4cca02b
Ajde <108722871+vratislavvv@users.noreply.github.com>
2023-11-02 19:16:05 +0000

    Basic django playing

--- folD/myapp/views.py
+++ folD/myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("hello world!")
+
+def dashboard(request):
+    return HttpResponse("tvoja mater")
