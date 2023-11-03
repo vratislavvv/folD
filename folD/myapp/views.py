48d6cc91cb6011dead1f5b1436fc32e519b0d7c7
Ajde <108722871+vratislavvv@users.noreply.github.com>
2023-11-03 07:22:22 +0000

    Rendering html template through django web app

--- folD/myapp/views.py
+++ folD/myapp/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return HttpResponse("hello world!")
 
+def template(request):
+    return render(request, "/workspaces/gradeproject/folD/myapp/templates/index.html", {})
+
 def dashboard(request):
-    return HttpResponse("tvoja mater")
+    return render(request, "/workspaces/gradeproject/folD/myapp/templates/layouts/layout.html", {})
