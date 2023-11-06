880e757a475937d9e6b6402fe1a9c54d23c6a0ca
Ajde <108722871+vratislavvv@users.noreply.github.com>
2023-11-06 10:39:31 +0000

    img folder relocation

--- folD/folD/settings.py
+++ folD/folD/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets"),
+    os.path.join(BASE_DIR, "folD/myapp/static"),
 ]
 
 # Default primary key field type
