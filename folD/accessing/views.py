6a61870f2a748b4147e4068b5605ba83b3ed2364
Ajde <108722871+vratislavvv@users.noreply.github.com>
2023-12-14 14:30:02 +0000

    Another big update

--- folD/accessing/views.py
+++ folD/accessing/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def login_user(request):
+    return render(request, 'authentication/login.html', {})
